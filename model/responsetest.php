89d067d9d654b7698f7515326f46b1fd499f52a7
Danylo <danilo.savchenko96@gmail.com>
2022-11-09 13:16:06 +0200

    DB connection

--- model/responsetest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('Response.php');
-
-$response = new Response();
-$response->setSuccess(false);
-$response->setHttpStatusCode(404);
-$response->addMessage("T3213213");
-$response->send();
